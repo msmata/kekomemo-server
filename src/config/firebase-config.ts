ffb5b0eff61c401edbf8081f0be2fac26abb9333
Martin Mata <martin.mata@grupoesfera.com.ar>
2022-06-02 18:00:29 -0300

    Extrae config de Firebase a archivo de configuracion

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-   apiKey: 'AIzaSyB8tWEL69iFWhKIm9YBbiFqDLVamnuS9h8',
-   authDomain: 'kekomemo-redux.firebaseapp.com',
-   projectId: 'kekomemo-redux',
-   storageBucket: 'kekomemo-redux.appspot.com',
-   messagingSenderId: '433049969369',
-   appId: '1:433049969369:web:4d4d2de20e7dac9a2c94c8',
+   apiKey: process.env.FIREBASE_API_KEY,
+   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.FIREBASE_PROJECT_ID,
+   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
